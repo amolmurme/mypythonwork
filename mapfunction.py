340faed545b032c62cd1344da9f95ade0ef46a1e
Amol Murme <39708474+amolmurme@users.noreply.github.com>
2021-11-21 15:55:48 +0530

    Create mapfunction.py

---
+++ mapfunction.py
@@ -0,0 +1,8 @@
+# List of String
+l=['sat','cat','bat']
+# map() can listify the list of strings individually
+temp=list(map(list,l))
+print(temp)
+###---OUTPUT--
+[['s','a','t'],['c','a','t'],['b','a','t']]
+###
